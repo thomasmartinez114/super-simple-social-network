c35de46d1ed6e8774fe7fe9683c0360b22eb4c18
Thomas Martinez <tmartinez114@gmail.com>
2024-06-07 12:01:25 -0400

    moved all to one file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,28 @@ import {
   createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom"
-import MainLayout from "./layouts/MainLayout"
-import HomePage from "./pages/HomePage"
-import CreatePost from "./pages/HomePage"
+// import MainLayout from "./layouts/MainLayout"
+// import HomePage from "./pages/HomePage"
+// import CreatePost from "./pages/HomePage"
+import Navbar from "./components/Navbar"
+import PostsAll from "./components/PostsAll"
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/create" element={<CreatePost />} />
-    </Route>
-  )
-)
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<MainLayout />}>
+//       <Route index element={<HomePage />} />
+//       <Route path="/create" element={<CreatePost />} />
+//     </Route>
+//   )
+// )
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <Navbar />
+      <PostsAll />
+    </>
+  )
 }
 
 export default App
